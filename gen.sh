c84d9511bbbb4eb6cb556e7d3fa1c72adb7392fc
mandlebro <casal.filipe@gmail.com>
2017-11-14 21:45:18 +0000

    added r2 commands to setup breakpoint

--- gen.sh
+++ gen.sh
@@ -4,5 +4,10 @@ python writec.py
 gcc gen.c -fno-stack-protector -z execstack -o shellcode
 rm ./gen.c
 chmod +x shellcode
-echo $'use this:\n\n >>>>>>>>>>     aaa; s main; db `pdf~call[0]`; dc\n\n'
-r2 -d shellcode
+clear
+echo '\n\n****************************************'
+echo '>>> breakpoint is at the shellcode call'
+echo '>>> use s to step forward           '
+echo '****************************************\n\n'
+
+r2 -d shellcode -c 'aaa; s main; db `pdf~call[0]`; dc'
